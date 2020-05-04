b199dbea96d53ca6b2350932c4960955db3d8966
Zachary Horvitz <zacharyhorvitz@users.noreply.github.com>
2020-05-04 03:42:53 -0400

    made buffer 1mil

--- run.sh
+++ run.sh
@@ -16,4 +16,4 @@
 # --model-path ./saved_models \
 # --num-frames 4
 
-python train.py --env "PongNoFrameskip-v4" --model-type cnn --gpu --seed 5 --lr 0.00025 --batchsize 32 --replay-buffer-size 100000 --warmup-period 30000 --max-steps 10000000 --test-policy-episodes 10 --reward-clip 0 --epsilon-decay 100000 --model-path ./saved_models --num-frames 4
+python train.py --env "PongNoFrameskip-v4" --model-type cnn --gpu --seed 5 --lr 0.00025 --batchsize 32 --replay-buffer-size 1000000 --warmup-period 30000 --max-steps 10000000 --test-policy-episodes 10 --reward-clip 0 --epsilon-decay 100000 --model-path ./saved_models --num-frames 4
