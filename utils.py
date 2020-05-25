5a3d293d1b20a1ab97e019f93f28b1a1dc3fc5d6
neevparikh <neev_parikh@brown.edu>
2020-05-25 19:19:10 -0400

    added back use_hier

--- utils.py
+++ utils.py
@@ -153,6 +153,10 @@ def parse_args():
                         type=int,
                         default=10,
                         required=False)
+    parser.add_argument('--use-hier',
+                        help='Use latent nodes',
+                        action='store_true',
+                        required=False)
     parser.add_argument('--mode',
                         help='select mode',
                         choices=["skyline", "ling_prior","embed_bl","cnn"],
